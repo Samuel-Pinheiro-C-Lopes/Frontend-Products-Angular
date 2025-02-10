75045cd0b32eb27cea7e53fed3971d7b9efaf6d4
Samuel-Pinheiro-C-Lopes <samuelpinheirocoutinholopes321@gmail>
2025-02-09 21:14:50 -0300

    Imported bootstrap styles to the project and added base html structure and validation for the product's component form

--- src/app/core/components/product/product.component.ts
+++ src/app/core/components/product/product.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { product } from '../../../shared/models/product.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product',
@@ -8,15 +9,32 @@ import { product } from '../../../shared/models/product.model';
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
 
-  p: product | undefined;
+  products: product[];
+  product: product;
+  sub: Subscription;
 
-  constructor(private sproduct: ProductService) {
-    sproduct.findById(1).subscribe({
-      next: (p) => this.p = p,
-      error: (err) => console.error(err),
-      complete: () => console.info("completed")
-    })
+  constructor(private service: ProductService) {
+    this.products = [];
+    this.product = new product();
+    this.sub = new Subscription();
   }
+
+  ngOnInit() {
+    this.sub.add(this.findAll());
+  }
+
+  findAll = (): Subscription => this.service.findAll().subscribe({
+    next: (products) => this.products = products,
+    error: (err) => console.error(err)
+  });
+
+  save = (product: product): Subscription => this.service.save(product).subscribe({
+    next: () => this.findAll(),
+    error: (err) => console.error(err)
+  });
+
+  clearSub = (): void => this.sub.unsubscribe();
+
 }
